a2917490a9439be48dafad2a66d050fb5f101c87
asato <sethuna03@gmail.com>
2019-11-26 22:19:19 +0900

    ディレクトリ整理

---
+++ ABC146/ABC146b.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class ABC146b{
+	public static void main(String args[]){
+		Scanner sc = new Scanner(System.in);
+		String S = sc.next();
+
+		char[] moji = S.toCharArray();
+
+		System.out.println((int)(moji[0]));
+	}
+}
