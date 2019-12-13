fa7aba96b69ba3eaa113c996ace7f6da9fe657d7
asato <sethuna03@gmail.com>
2019-12-14 00:26:04 +0900

    ABC146Cを解説見てからクリアした

--- ABC146/ABC146cX.java
+++ ABC146/ABC146cX.java
@@ -1,45 +1,54 @@
 import java.util.*;
 
+/*
+   キャストを多用したくないので変数は基本longで扱う。
+ */
 public class ABC146cX{
 	public static void main(String args[]){
 		Scanner sc = new Scanner(System.in);
-		long A = sc.nextLong();
-		long B = sc.nextLong();
+		long A = sc.nextInt();
+		long B = sc.nextInt();
 		long X = sc.nextLong();
-		long N = 0;
-		if(canBuy(calc(A, B, N), X)){
-			N = 1;
-		}
-		System.out.println(calc(A, B, binarySearch(N, A, B, X)));
+		long N;
+
+		N = calcMaximumPrice(A, B, X);
+
+		System.out.println(N);
 	}
 
-	public static long binarySearch(long N, long A, long B, long X){
-		long lower = N;
-		long upper = N * 10 - 1; 
-		long mid = lower;
+	public static long calcMaximumPrice(long A, long B, long X){
+		//予算で買える最大の整数を計算
+		//二部探査で割り出す
+		long right = 1000000000;//10^9
+		long left = 0;
+		long N = 1;
 
-		for(int i = 0; i < 15; i++){
-			mid = (upper + lower) / 2;
-			if(canBuy(calc(A, B, mid), X)){
-				lower = mid;
-			}else {
-				upper = mid;
+		if(calcPrice(A, B, right) <= X)return right;
+		if(calcPrice(A, B, N) > X )return 0;
+		while(left + 1 < right){
+			N = (left + right) / 2;
+			if(calcPrice(A, B, N) < X){//答えがNより右側にあることがわかったら
+				left = N;//左をNまで寄せる
+			}else if(calcPrice(A, B, N) > X){//答えがNより左側にあることがわかった
+				right = N;//右側をNまで寄せる
+			}else{
+				return N;
 			}
 		}
+		if(calcPrice(A, B, N) > X)N --;
+		return N;
 
-		return mid;
 	}
 
-	public static boolean canBuy(long price, long myMoney){
-		if(price <= myMoney)return true;
-		return false;
+	public static long calcKeta(long N){
+		//Nの桁数を計算
+		long keta;
+		keta = String.valueOf(N).length();
+		return keta;
 	}
 
-	public static long calc(long A, long B, long N){
-		return A*N+B*(long)(calcKeta(N));
-	}
-
-	public static int calcKeta(long N){
-		return (int)(String.valueOf(N).length());
+	public static long calcPrice(long A, long B, long N){
+		//値段を計算
+		return A * N + B * calcKeta(N);
 	}
 }
